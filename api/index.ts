8fefad5876c027c2187426c7ddc44b3578220f3b
jotavv620 <jv0261324@gmail.com>
2026-03-27 11:25:42 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,3 @@
-import server from '../server/_core/index';
+// Adicionamos a extensão .ts ou .js para ajudar o mestre de obras a achar o arquivo exato
+import server from '../server/_core/index.ts'; 
 export default server;
